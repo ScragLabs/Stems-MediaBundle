712ffd273282ae5756ed67b58d9f0f9b1fc1b988
Stephen Wilkinson <du_monde@hotmail.com>
2014-08-29 11:34:09 +0000

    Decoupled image entity from type entity as its not needed

--- Entity/Image.php
+++ Entity/Image.php
@@ -25,6 +25,16 @@ class Image
      */
     protected $filename;
 
+    /** 
+     * @ORM\Column(type="string")
+     */
+    protected $src;
+
+    /** 
+     * @ORM\Column(type="string")
+     */
+    protected $category = 'uncategorised';
+
     /**
      * @ORM\Column(type="string") 
      */
@@ -45,14 +55,180 @@ class Image
      */
     protected $updated;
 
-    /**
-     * @ORM\OneToMany(targetEntity="Type", mappedBy="image")
-     */
-    protected $types; 
-
     public function __construct()
     {
         $this->created = new \DateTime;
         $this->updated = new \DateTime;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set filename
+     *
+     * @param string $filename
+     * @return Image
+     */
+    public function setFilename($filename)
+    {
+        $this->filename = $filename;
+    
+        return $this;
+    }
+
+    /**
+     * Get filename
+     *
+     * @return string 
+     */
+    public function getFilename()
+    {
+        return $this->filename;
+    }
+
+    /**
+     * Set src
+     *
+     * @param string $src
+     * @return Image
+     */
+    public function setSrc($src)
+    {
+        $this->src = $src;
+    
+        return $this;
+    }
+
+    /**
+     * Get src
+     *
+     * @return string 
+     */
+    public function getSrc()
+    {
+        return $this->src;
+    }
+
+    /**
+     * Set category
+     *
+     * @param string $category
+     * @return Image
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+    
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return string 
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * Set mime
+     *
+     * @param string $mime
+     * @return Image
+     */
+    public function setMime($mime)
+    {
+        $this->mime = $mime;
+    
+        return $this;
+    }
+
+    /**
+     * Get mime
+     *
+     * @return string 
+     */
+    public function getMime()
+    {
+        return $this->mime;
+    }
+
+    /**
+     * Set deleted
+     *
+     * @param boolean $deleted
+     * @return Image
+     */
+    public function setDeleted($deleted)
+    {
+        $this->deleted = $deleted;
+    
+        return $this;
+    }
+
+    /**
+     * Get deleted
+     *
+     * @return boolean 
+     */
+    public function getDeleted()
+    {
+        return $this->deleted;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return Image
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set updated
+     *
+     * @param \DateTime $updated
+     * @return Image
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+    
+        return $this;
+    }
+
+    /**
+     * Get updated
+     *
+     * @return \DateTime 
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
     }
 }
